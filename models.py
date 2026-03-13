f9aa0fb1e719afa9decb5ff897db508ee920b7c4
kavin0410 <kavingiri2006@gmail.com>
2026-03-13 20:22:19 +0530

    Merge branch 'main' of https://github.com/makeshkumar14/AI-project-face-recognition-

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ MYSQL_CONFIG = {
     'host': os.environ.get('MYSQL_HOST', 'localhost'),
     'port': int(os.environ.get('MYSQL_PORT', 3306)),
     'user': os.environ.get('MYSQL_USER', 'root'),
-    'password': os.environ.get('MYSQL_PASSWORD', ''),
+    'password': os.environ.get('MYSQL_PASSWORD', 'admin'),
     'database': os.environ.get('MYSQL_DATABASE', 'face_recognition_db'),
 }
 
