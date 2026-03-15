a2d726ddfd97a5435d80c19cc6fe95a4f53f24e2
makeshkumar14 <makesh.pyo@gmail.com>
2026-03-15 18:52:18 +0530

    edited  scroll animation

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ MYSQL_CONFIG = {
     'host': os.environ.get('MYSQL_HOST', 'localhost'),
     'port': int(os.environ.get('MYSQL_PORT', 3306)),
     'user': os.environ.get('MYSQL_USER', 'root'),
-    'password': os.environ.get('MYSQL_PASSWORD', 'ratchu@2007'),
+    'password': os.environ.get('MYSQL_PASSWORD', 'makesh14'),
     'database': os.environ.get('MYSQL_DATABASE', 'face_recognition_db'),
 }
 
